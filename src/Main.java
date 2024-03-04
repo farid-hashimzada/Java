a045de7307da3c26207a48c35d5e037798afaa0b
Farid Hashimzada <hashimzadafarid@gmail.com>
2024-03-04 18:05:19 +0400

    Java fundemental

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Farid");
+        System.out.println("Seyyar");
+        System.out.println("fdrfr");
+        hazelnet();
+    }
+    public static void hazelnet() {
+        System.out.println("Tree");
+        System.out.println("Grass");
+
+    }
+}
