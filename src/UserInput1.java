fda070d573c4a17c8bfdd6ec18fd71eb39b1a38e
Farid Hashimzade <124304902+farid-hashimzada@users.noreply.github.com>
2024-03-04 18:34:47 +0400

    Update UserInput1.java

--- src/UserInput1.java
+++ src/UserInput1.java
@@ -1,2 +1,13 @@
 package PACKAGE_NAME;public class UserInput1 {
+  import java.util.Scanner;
+
+public class UserInput1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter something");
+        String input = sc.nextLine();
+        System.out.printf("You entered %s", input);
+    }
+}
+
 }
