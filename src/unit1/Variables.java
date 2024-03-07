a92df88677981a30be5b90701f2a2531f490b1fc
Farid Hashimzada <hashimzadafarid@gmail.com>
2024-03-07 17:54:16 +0400

    Java fundemental

--- src/unit1/Variables.java
+++ src/unit1/Variables.java
@@ -1,2 +1,19 @@
-package unit1;public class Variables {
+package unit1;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        double floo = 5.5;
+        int bar = (int)floo;
+
+        System.out.println(floo);
+        System.out.println(bar);
+
+//        int;
+//        String;
+//        float;
+//        short;
+//        long;
+        //double
+    }
 }
--- src/unit1/sec5_user_input/ScannerInput.java
+++ src/unit1/sec5_user_input/ScannerInput.java
@@ -1,2 +1,34 @@
-package unit1.sec5_user_input;public class ScannerInput {
+package unit1.sec5_user_input;
+import java.util.Scanner;
+public class ScannerInput {
+    public static void main(String[] args) {
+        int age = 27;
+        double statueInternetConnection = 14.72;
+        boolean hasInternetConnection = false;
+//        String userInput = "\u005A";
+//        String name = "Jeff";
+//        Scanner scanner = new Scanner(System.in);
+//        String input =  scanner.next();
+//        System.out.printf(input);
+        userInput();
+    }
+
+    private static void userInput() {
+        System.out.println("What is your name");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
+        System.out.println(userInput + " is a nice name");
+
+        System.out.println("How old are you");
+        int userAge = scanner.nextInt();
+        System.out.println("Your name is " + userInput + " and your age " + userAge);
+        System.out.println("\n how tall are you in metres");
+        double userHeight = scanner.nextDouble();
+        System.out.println("You are " + userHeight);
+        StringFormatter()
+    }
+
+    private static void StringFormatter {
+        System.out.format("i have %.3f cats, %d dogs, and 1 %s.", 7.9, 2, "panther");
+    }
 }
